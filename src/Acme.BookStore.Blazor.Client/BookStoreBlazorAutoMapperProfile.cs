608d81e6316e7f8d22ce4919cd53b4e83243cf53
Sang <75106540+sang1833@users.noreply.github.com>
2025-03-19 11:57:43 +0700

    Add project files.

---
+++ src/Acme.BookStore.Blazor.Client/BookStoreBlazorAutoMapperProfile.cs
@@ -0,0 +1,17 @@
+using Acme.Bookstore.Books;
+using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using AutoMapper;
+
+namespace Acme.BookStore.Blazor.Client;
+
+public class BookStoreBlazorAutoMapperProfile : Profile
+{
+    public BookStoreBlazorAutoMapperProfile()
+    {
+        //Define your AutoMapper configuration here for the Blazor project.
+        CreateMap<BookDto, CreateUpdateBookDto>();
+        CreateMap<AuthorDto, UpdateAuthorDto>();
+
+    }
+}
---
+++ src/Acme.BookStore.Blazor.Client/BookStoreStyleBundleContributor.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace Acme.BookStore.Blazor.Client;
+
+public class BookStoreStyleBundleContributor : BundleContributor
+{
+    public override void ConfigureBundle(BundleConfigurationContext context)
+    {
+        context.Files.Add(new BundleFile("main.css", true));
+    }
+}
